e6b591e23e9a3c7a17008aa59c4d9e2caaac0484
amin-salpagarov <ptyhondev@gmail.com>
2024-04-29 22:48:32 +0300

    after chapter 4.5

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -5,6 +5,7 @@ import (
 	"github.com/amin-salpagarov/greenlight/internal/data"
 	"net/http"
 	"time"
+	"github.com/amin-salpagarov/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +18,17 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
